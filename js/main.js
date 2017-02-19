57d18c1c28d4afa116d501cebc6f4594f9eb041f
Mindy McAdams <mmcadams@gmail.com>
2017-02-19 09:23:34 -0500

    add functions to UI
    separate habitat table and dragon entry area; add jQuery to handle
    those; get selected habitat from table row click

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
 
-
     buildHabitatTable();
     $('#habitatTable').DataTable({
       paging: false,
       searching: false
     });
 
+    $('#dragons').hide();
+
     // habitats array comes from dragondata.json
     function buildHabitatTable() {
         for (var i = 0; i < habitats.length; i++) {
             var row = '';
-            row += '<tr id="' + habitats[i].dname + '">';
-            row += '<td>' + habitats[i].dname + '</td>';
+            row += '<tr data-key="' + i + '">';
+            row += '<td><a href="#">' + habitats[i].dname + '</a></td>';
             row += '<td>' + habitats[i].max_cash + '</td>';
             row += '<td>' + habitats[i].max_dragons + '</td>';
             row += '<td>' + habitats[i].level_avail + '</td>';
@@ -21,5 +22,31 @@ $(document).ready(function() {
             $('#habitatTable tbody').append(row);
         }
     }
+
+    // listen for click on any row
+    $('#habitatTable tbody tr').on('click', function(e){
+      e.preventDefault();
+      // jQuery data attribute in tr holds index of hab in JSON array
+      var habIndex = $(this).data('key');
+      var habObject = habitats[habIndex];
+      // hide table, show form fields
+      $('#selectHab').slideUp();
+      $('#dragons').slideDown();
+      // write data for selected hab into dt elements
+      // ul li:nth-child(2)
+      $('#dragons h2').text(habObject.dname);
+      $('#dragons dl dd span').text(habObject.max_cash);
+      $('#dragons dl dd:nth-child(4)').text(habObject.max_dragons);
+      $('#dragons dl dd:nth-child(6)').text(habObject.level_avail);
+      $('#dragons dl dd:nth-child(8)').text(habObject.dragon_types);
+    });
+
+    // listen for click on link
+    $('#backToTable').on('click', function(e){
+      e.preventDefault();
+      $('#selectHab').slideDown();
+      $('#dragons').hide();
+    });
+
 
 }); // close document ready
